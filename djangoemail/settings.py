2f4ffc99a097519dc899738b10a52ed331fc1e69
root <root@DESKTOP-FDL90FV.localdomain>
2023-05-15 08:31:51 +0300

    first commit

--- djangoemail/settings.py
+++ djangoemail/settings.py
@@ -125,9 +125,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'zolatechnologies@gmail.com'
-EMAIL_HOST_PASSWORD = 'rfcxtngibqxucfyq'
+EMAIL_HOST_USER = 'email address'
+EMAIL_HOST_PASSWORD = 'password'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
-DEFAULT_FROM_EMAIL = 'zolatechnologies@gmail.com'
+DEFAULT_FROM_EMAIL = 'email address'
